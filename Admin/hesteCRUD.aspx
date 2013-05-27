d4e1d19a6bf8872907e51df8764d601f661f3f38
Søren Magnusson <sm@rts.dk>
2013-05-27 17:33:27 +0200

    CRUD med EF, 1-0-1

---
+++ Admin/hesteCRUD.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="hesteCRUD.aspx.cs" Inherits="Admin_hesteCRUD" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+    
+    div#liste, div#edit
+    {
+        float:left;
+    }
+    
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div id="liste">
+            <asp:Button ID="ButtonNy" runat="server" onclick="ButtonNy_Click" 
+                Text="Ny Hest" />
+            <br />
+            <asp:EntityDataSource ID="EntityDataSourceHeste" runat="server" 
+                ConnectionString="name=RideklubbenContext" 
+                DefaultContainerName="RideklubbenContext" EnableFlattening="False" 
+                EntitySetName="Heste">
+            </asp:EntityDataSource>
+            <asp:Repeater ID="RepeaterHeste" runat="server" 
+                DataSourceID="EntityDataSourceHeste" 
+                onitemcommand="RepeaterHeste_ItemCommand">
+                <ItemTemplate>
+                    <div class="hest">
+                        <asp:LinkButton ID="LinkButtonHest" runat="server" CommandName="hentHest" CommandArgument='<%# Eval("HesteId") %>'>
+                        <asp:Image ID="Image1" runat="server" ImageUrl='<%# "../" + Eval("BilledeSti") %>' Width="100" />
+                        <span class="hesteNavn"><%# Eval("Navn") %></span>
+                        </asp:LinkButton>
+                        <asp:LinkButton ID="LinkButtonSlet" runat="server" CommandName="sletHest" CommandArgument='<%# Eval("HesteId") %>'>Slet</asp:LinkButton>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+        <div id="edit">
+            <asp:Panel ID="PanelEdit" runat="server" Visible="false" >
+                <asp:Label ID="LabelNavn" runat="server" BackColor="#F0F0F0" Text="Navn" 
+                    Width="200px"></asp:Label>
+                <asp:TextBox ID="TextBoxNavn" runat="server" Width="200px"></asp:TextBox>
+                <br />
+                <asp:Label ID="LabelVægt" runat="server" BackColor="#F0F0F0" Text="Vægt" 
+                    Width="200px"></asp:Label>
+                <asp:TextBox ID="TextBoxVægt" runat="server" Width="200px"></asp:TextBox>
+                <br />
+                <asp:Label ID="LabelHøjde" runat="server" BackColor="#F0F0F0" Text="Højde" 
+                    Width="200px"></asp:Label>
+                <asp:TextBox ID="TextBoxHøjde" runat="server" Width="200px"></asp:TextBox>
+                <br />
+                <asp:Button ID="ButtonOpdater" runat="server" Text="Opdater" 
+                    onclick="ButtonOpdater_Click" Visible="false" />
+                <asp:Button ID="ButtonOpret" runat="server" Text="Opret" 
+                    onclick="ButtonOpret_Click" Visible="false" />
+                
+            </asp:Panel>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Admin/hesteCRUD.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_hesteCRUD : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void RepeaterHeste_ItemCommand(object source, RepeaterCommandEventArgs e)
+    {
+        if (e.CommandName == "hentHest")
+        {
+            int hesteID = Convert.ToInt32(e.CommandArgument);
+            Hest hesten;
+
+            using (var ctx = new RideklubbenContext())
+            {
+                hesten = ctx.Heste.Single(h => h.HesteId == hesteID);
+
+                TextBoxNavn.Text = hesten.Navn;
+                TextBoxVægt.Text = hesten.Vægt.ToString();
+                TextBoxHøjde.Text = hesten.Højde.ToString();
+                ViewState["hesteID"] = hesteID;
+            }
+
+            PanelEdit.Visible = true;
+            ButtonOpdater.Visible = true;
+            ButtonOpret.Visible = false;
+        }
+        else if (e.CommandName == "sletHest")
+        {
+            int hesteID = Convert.ToInt32(e.CommandArgument);
+            Hest hesten;
+
+            using (var ctx = new RideklubbenContext())
+            {
+                // indlæs entiteten
+                hesten = ctx.Heste.Single(h => h.HesteId == hesteID);
+                // slet entiteten
+                ctx.DeleteObject(hesten);
+                // gem ændringerne
+                ctx.SaveChanges();
+            }
+            RepeaterHeste.DataBind();
+        }
+    }
+
+    protected void ButtonOpdater_Click(object sender, EventArgs e)
+    {
+        int hesteID = Convert.ToInt32(ViewState["hesteID"]);
+        Hest hesten;
+        using (var ctx = new RideklubbenContext())
+        {
+            // vi henter den entitet der skal redigeres
+            hesten = ctx.Heste.Single(h => h.HesteId == hesteID);
+
+            // nye værdier puttes ind i objektet
+            hesten.Navn = TextBoxNavn.Text;
+            hesten.Vægt = Convert.ToDouble( TextBoxVægt.Text );
+            hesten.Højde = Convert.ToDouble( TextBoxHøjde.Text );
+            
+            // her er magien
+            ctx.SaveChanges();
+        }
+        PanelEdit.Visible = false;
+        ButtonOpdater.Visible = false;
+        RepeaterHeste.DataBind();
+    }
+
+    protected void ButtonNy_Click(object sender, EventArgs e)
+    {
+        TextBoxNavn.Text = "";
+        TextBoxVægt.Text = "";
+        TextBoxHøjde.Text = "";
+
+        PanelEdit.Visible = true;
+        ButtonOpret.Visible = true;
+        ButtonOpdater.Visible = false;
+    }
+
+    // ny hest
+    protected void ButtonOpret_Click(object sender, EventArgs e)
+    {
+        // hesten oprettes, erklæres som variabel og instantieres med default constructor
+        Hest hesten = new Hest();
+        using (var ctx = new RideklubbenContext())
+        {
+            // værdier puttes ind i objektet
+            hesten.Navn = TextBoxNavn.Text;
+            hesten.Vægt = Convert.ToDouble(TextBoxVægt.Text);
+            hesten.Højde = Convert.ToDouble(TextBoxHøjde.Text);
+
+            // Åh suk. Jeg har lavet disse felter "not null" i databasen, så der skal være en værdi i.
+            hesten.Forælder_HesteId = ctx.Heste.Single(f => f.Navn == "Stamfar").HesteId;
+            hesten.Fødestald = "";
+            hesten.FødeDato = DateTime.Now;
+            hesten.BilledeSti = "";
+
+            // her er magien
+            ctx.Heste.AddObject(hesten);
+            ctx.SaveChanges();
+        }
+        PanelEdit.Visible = false;
+        ButtonOpret.Visible = false;
+        RepeaterHeste.DataBind();
+    }
+}
